1e30cb75cd397f7de61add04690d7b8047a51654
tfranssen <thijsfranssen@gmail.com>
2023-04-07 10:52:06 +0200

    update 6 sensoren

--- GruttoKar.ino
+++ GruttoKar.ino
@@ -4,7 +4,7 @@
 #define ENCODER_USE_INTERRUPTS
 #include <Encoder.h>
 
-#define ENCODER_PULSES_PER_METER 4481
+#define ENCODER_PULSES_PER_METER 5300
 #define ENCODER_ROUNDING 3
 //every 2 centimeters, the decoder distance is written to the serial bus
 #define UPDATE_DISTANCE_METER 0.01
@@ -19,27 +19,25 @@
 
 Encoder encoder(CONTROLLINO_IN0, CONTROLLINO_IN1);
 
-//char laser_pins[] = {CONTROLLINO_A5, CONTROLLINO_A4, CONTROLLINO_A0, CONTROLLINO_A3, CONTROLLINO_A2, CONTROLLINO_A1};
-char laser_pins[] = {CONTROLLINO_A0, CONTROLLINO_A3, CONTROLLINO_A2, CONTROLLINO_A1};
-
+char laser_pins[] = {CONTROLLINO_A5, CONTROLLINO_A4, CONTROLLINO_A3, CONTROLLINO_A2, CONTROLLINO_A0, CONTROLLINO_A1};
 const int NUM_PINS = sizeof(laser_pins);
 
 char prev_laser_data[NUM_PINS] = {0};
-long prev_encoder=0;
-bool debug_mode = false;
+long prev_encoder = 0;
+bool debug_mode = true;
 
 long debug_prev_encoder = -2147483647; //set long to max value to force value change
 
 void setup() {
   // put your setup code here, to run once:
-  for (int i=0; i<NUM_PINS; i++){
+  for (int i = 0; i < NUM_PINS; i++) {
     pinMode(laser_pins[i], INPUT);
   }
-  Serial.begin(115200); 
+  Serial.begin(115200);
 }
 
-float encoder_convert_to_meter(long encoder_ticks){
-  return float(encoder_ticks)/ENCODER_PULSES_PER_METER;
+float encoder_convert_to_meter(long encoder_ticks) {
+  return float(encoder_ticks) / ENCODER_PULSES_PER_METER;
 }
 
 String encoder_float_to_string(float encoder)
@@ -56,7 +54,7 @@ void handle_sensors (void) {
 
   data_changed = read_laser_sensors(sensor_data);
   if (data_changed) {
-    for (int i=0; i<NUM_PINS; i++){
+    for (int i = 0; i < NUM_PINS; i++) {
       Serial.print(sensor_data[i]);
       Serial.print(" ");
     }
@@ -66,7 +64,7 @@ void handle_sensors (void) {
   } else {
     //laser data hasn't changed, check if encoder data should be send;
     encoder_status = read_encoder(&encoder_distance);
-    if (encoder_status){
+    if (encoder_status) {
       Serial.print(":");
       encoder_s = encoder_float_to_string(encoder_distance);
       Serial.println(encoder_s);
@@ -76,9 +74,9 @@ void handle_sensors (void) {
 
 void debug_encoder_status(long encoder)
 {
-  if (encoder != debug_prev_encoder){
+  if (encoder != debug_prev_encoder) {
     Serial.print(CMD_RESPONSE_CHAR); //debug data hidden in reponse to not effect other parts of software
-    Serial.print("[Debug][Encoder] "); 
+    Serial.print("[Debug][Encoder] ");
     Serial.println(encoder);
     debug_prev_encoder = encoder;
   }
@@ -88,11 +86,11 @@ bool read_encoder(float *encoder_distance)
 {
   long current_encoder = encoder.read();
 
-  if (debug_mode){
+  if (debug_mode) {
     debug_encoder_status(current_encoder);
   }
-  
-  if (current_encoder > prev_encoder+UPDATE_DISTANCE_TICKS){
+
+  if (current_encoder > prev_encoder + UPDATE_DISTANCE_TICKS) {
     *encoder_distance = encoder_convert_to_meter(current_encoder);
     prev_encoder = current_encoder;
     return true;
@@ -101,12 +99,22 @@ bool read_encoder(float *encoder_distance)
   }
 }
 
-bool read_laser_sensors(uint8_t *sensor_data){
+bool read_laser_sensors(uint8_t *sensor_data) {
   bool data_changed = false;
-  for (int i=0; i<NUM_PINS; i++){
-    char laser_status = !digitalRead(laser_pins[i]);
+  for (int i = 0; i < NUM_PINS; i++) {
+    char laser_status = 0;
+    if (i > 1) {
+      laser_status = !digitalRead(laser_pins[i]);
+    } else {
+      if (analogRead(laser_pins[i])>100) {
+        laser_status = 0;
+      } else
+      {
+        laser_status = 1;
+      }
+    }
     sensor_data[i] = laser_status;
-    if (laser_status != prev_laser_data[i]){
+    if (laser_status != prev_laser_data[i]) {
       data_changed = true;
     }
   }
@@ -138,16 +146,16 @@ void handle_command (void) {
   }
 }
 
-void reset(void){
+void reset(void) {
   encoder.write(0);
-  prev_encoder=0;
+  prev_encoder = 0;
   memset(prev_laser_data, 0, NUM_PINS);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   handle_sensors();
-  if (Serial.available()){
+  if (Serial.available()) {
     handle_command();
   }
 }
